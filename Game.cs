a9b9ea0b1e89728deda32e30c0303349b5466209
Vadim Kazakov <ivk060118@gmail.com>
2024-12-08 18:42:56 +0700

    Добавьте файлы проекта.

---
+++ Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_pattern
+{
+    public class Game
+    {
+        private Player player;
+
+        public Game()
+        {
+            player = new Player(new Sword());
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nChoose your weapon:");
+                Console.WriteLine("1. Sword");
+                Console.WriteLine("2. Bow");
+                Console.WriteLine("3. Axe");
+                Console.WriteLine("0. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        player.SetWeapon(new Sword());
+                        break;
+                    case "2":
+                        player.SetWeapon(new Bow());
+                        break;
+                    case "3":
+                        player.SetWeapon(new Axe());
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice, try again.");
+                        continue;
+                }
+
+                player.Attack();
+            }
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_pattern
+{
+    public class Player
+    {
+        private IWeapon weapon;
+
+        public Player(IWeapon weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        public void SetWeapon(IWeapon weapon)
+        {
+            this.weapon = weapon;
+            Console.WriteLine($"Switched to {weapon.GetType().Name}");
+        }
+
+        public void Attack()
+        {
+            weapon.UseWeapon();
+        }
+    }
+}
